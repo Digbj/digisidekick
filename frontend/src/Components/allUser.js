c5c34647bf572f4b7100a915739dec7c91c8be47
Digvijay <digbjssn@gmail.com>
2023-07-16 20:11:57 +0530

    search functionality

--- frontend/src/Components/allUser.js
+++ frontend/src/Components/allUser.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 function AllUser() {
   const [allUser, setAllUser] = useState([]);
   const [editedUserId, setEditedUserId] = useState(null);
+  const [search,setSearch]=useState('')
+  const [data1,setData1]=useState([])
 
   const fetchUsers = async () => {
     try {
@@ -68,8 +70,25 @@ function AllUser() {
     }
   };
 
+
+  //Search functionality
+  const Search = () => {
+    const filteredUsers = allUser.filter((user) =>
+      user.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setData1(filteredUsers);
+  };
+
   return (
     <div className='table'>
+        {/* search functionality */}
+        <div><input placeholder='Search By Name' onChange={(e)=>{setSearch(e.target.value)}}/> <button onClick={Search}>Search</button></div>
+        {data1.map((serUser,index)=>{
+            return(
+                <span key={index}><span>{serUser.name}</span> <span>{serUser.email}</span> <span>{serUser.phone}</span> <span>{serUser.address}</span></span>
+            )
+        })}
+        {/* maintable */}
       <table>
         <thead>
           <tr>
@@ -150,13 +169,13 @@ function AllUser() {
               <td>
                 {editedUserId === user._id ? (
                   <>
-                    <button onClick={() => handleSave(user._id)}>Save</button>
-                    <button onClick={() => setEditedUserId(null)}>Cancel</button>
+                    <button id='save' onClick={() => handleSave(user._id)}>Save</button>
+                    <button id='cancle' onClick={() => setEditedUserId(null)}>Cancel</button>
                   </>
                 ) : (
                   <>
-                    <button onClick={() => handleDelete(user._id)}>Delete</button>
-                    <button onClick={() => handleEdit(user._id)}>Edit</button>
+                    <button id='del' onClick={() => handleDelete(user._id)}>Delete</button>
+                    <button id='edi' onClick={() => handleEdit(user._id)}>Edit</button>
                   </>
                 )}
               </td>
