e46dc1fcdce6359a8e606801f556533b97b45aa2
Digvijay <digbjssn@gmail.com>
2023-07-16 20:20:25 +0530

    modified

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className='home'>
       <div className='det'>
-        <p>This app Lets u add <span>New User</span> in to Your user List and and can perform the CRUD operation on <span>Existing Users</span> </p>
+        <p>This app Lets you add <span>New User</span> in to Your user List and and can perform the CRUD operation on <span>Existing Users</span> </p>
       </div>
      <Link to='/AddUser'><button className='mainb'>Add User</button></Link> 
     </div>
